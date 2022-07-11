eeb0c73d8fd1302a3d89cf273628457521b58d97
Michael Puskas <mmpuskas@gmail.com>
2022-07-10 19:28:56 -0700

    Added back the array-based implementation, alongside the vector-based.

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -1,20 +1,182 @@
 #pragma once
 
+#include <array>
 #include <vector>
 #include <stdexcept>
 #include <algorithm>
+#include <type_traits>
 
 namespace AM {
 
+template<typename T>
+struct always_false : std::false_type {};
+
+
 /**
- * CircularBuffer
- *
  * A simple, exceptionally not thread-safe circular buffer.
  *
- * Positively indexes the previous values (e.g. if you pushed 3 values, they will be at
- * arr[0], arr[1], and arr[2], with arr[0] being the most recent).
+ * Positively indexes the previous values (e.g. if you pushed 3 values, they
+ * will be at arr[0], arr[1], and arr[2], with arr[0] being the most recent).
  */
-template <typename T> class CircularBuffer {
+template<typename T, std::size_t... Is>
+class CircularBuffer
+{
+    static_assert(always_false<T>::value,
+                  "CircularBuffer only supports 1 or 2 template arguments.");
+};
+
+/**
+ * Specialization for static-sized array as underlying container.
+ */
+template<typename T, std::size_t maxSize>
+class CircularBuffer<T, maxSize>
+{
+public:
+    typedef T value_type;
+    typedef T& reference;
+    typedef const T& const_reference;
+    typedef std::size_t size_type;
+
+    CircularBuffer()
+    : valueArr{}
+    , head(arrLen - 1)
+    {
+    }
+
+    CircularBuffer(value_type initialValue)
+    : head(arrLen - 1)
+    {
+        valueArr.fill(initialValue);
+    }
+
+    CircularBuffer(const CircularBuffer& other)
+    : head(other.head)
+    {
+        valueArr = other.valueArr;
+    }
+
+    CircularBuffer& operator=(const CircularBuffer& other)
+    {
+        // Check for self assignment.
+        if (this != &other) {
+            head = other.head;
+            valueArr = other.valueArr;
+        }
+
+        return *this;
+    }
+
+    /**
+     * Pushes elements into the buffer.
+     *
+     * @param value  The value to push.
+     * @post value has been copied to index 0.
+     *       The oldest value has been written over.
+     *       All other previous values are found at their old index + 1.
+     */
+    void push(const value_type value)
+    {
+        // Move the head, overwriting any old data.
+        head = decrement(head);
+
+        // Write the value
+        valueArr[head] = value;
+    }
+
+    template<class... Args>
+    void emplace_ctor(Args&&... args)
+    {
+        // Move the head, overwriting any old data.
+        head = decrement(head);
+
+        // Write the value
+        valueArr[head] = value_type(args...);
+    }
+
+    template<class... Args>
+    void emplace_aggregate(Args&&... args)
+    {
+        // Move the head, overwriting any old data.
+        head = decrement(head);
+
+        // Write the value
+        valueArr[head] = {args...};
+    }
+
+    /**
+     * Wrapping access to the buffer.
+     * @throws std::out_of_range if index is larger than maxSize.
+     */
+    reference operator[](size_type index)
+    {
+        if (index >= arrLen) {
+            throw std::out_of_range("index larger than maxSize - 1.");
+        }
+
+        size_type requestedIndex = increment(head, index);
+        return valueArr[requestedIndex];
+    }
+    const_reference operator[](size_type index) const
+    {
+        if (index >= arrLen) {
+            throw std::out_of_range("index larger than maxSize - 1.");
+        }
+
+        size_type requestedIndex = increment(head, index);
+        return valueArr[requestedIndex];
+    }
+
+    /**
+     * Returns the length of the underlying array.
+     */
+    size_type size() const { return arrLen; }
+
+    /**
+     * Fills all elements in the underlying container with the given value.
+     */
+    void fill(const value_type value) { valueArr.fill(value); }
+
+private:
+    /**
+     * The length of the underlying array.
+     */
+    static constexpr size_type arrLen = maxSize;
+
+    /** The data structure that holds the queue's elements. */
+    std::array<value_type, arrLen> valueArr;
+
+    /**
+     * Index of the most recently inserted element.
+     * Starts at the end of the underlying array and gets decremented during
+     * every push.
+     */
+    size_type head;
+
+    //----------------------------------------------------------------------------
+    // Convenience Functions
+    //----------------------------------------------------------------------------
+    /**
+     * Returns the index, incremented by amount. Accounts for wrap-around.
+     */
+    size_type increment(const size_type index, const size_type amount) const
+    {
+        return (index + amount) % arrLen;
+    }
+
+    /**
+     * Returns the previous index. Accounts for wrap-around.
+     * The returned value is undefined for indices > arrLen.
+     */
+    size_type decrement(const size_type index) const
+    {
+        return (index + arrLen - 1) % arrLen;
+    }
+};
+
+/**
+ * Specialization for dynamic vector as underlying container.
+ */
+template <typename T> class CircularBuffer<T> {
 public:
     typedef T value_type;
     typedef T& reference;
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -17,7 +17,80 @@ bool compareBufferContents(CircularBuffer<T>& buffer,
     return true;
 }
 
-int main() {
+template<typename T, std::size_t maxSize>
+bool compareBufferContents(CircularBuffer<T, maxSize>& buffer,
+                           std::vector<T>& compareVector)
+{
+    for (unsigned int i = 0; i < maxSize; ++i) {
+        if (buffer[i] != compareVector[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void testArrayBased()
+{
+    // Size is correct.
+    CircularBuffer<int, 2024> buffer1;
+    assert(buffer1.size() == 2024);
+
+    // Initialization works.
+    CircularBuffer<int, 5> buffer2(-1);
+
+    std::vector<int> compareVector = {-1, -1, -1, -1, -1};
+    assert(compareBufferContents(buffer2, compareVector));
+
+    // 4 Elements in order.
+    CircularBuffer<int, 4> buffer3;
+    for (unsigned int i = 0; i < 4; ++i) {
+        buffer3.push(i);
+    }
+
+    compareVector = {3, 2, 1, 0};
+    assert(compareBufferContents(buffer3, compareVector));
+
+    // 5 Elements in order (1 wrap).
+    CircularBuffer<int, 4> buffer4;
+    for (unsigned int i = 0; i < 5; ++i) {
+        buffer4.push(i);
+    }
+
+    compareVector = std::vector({4, 3, 2, 1});
+    assert(compareBufferContents(buffer4, compareVector));
+
+    // 100 Elements in order (96 wraps).
+    CircularBuffer<int, 4> buffer5;
+    for (unsigned int i = 0; i < 100; ++i) {
+        buffer5.push(i);
+    }
+
+    compareVector = std::vector({99, 98, 97, 96});
+    assert(compareBufferContents(buffer5, compareVector));
+
+    // Random stuff from the example.
+    CircularBuffer<int, 3> buffer6(10);
+    compareVector = std::vector({10, 10, 10});
+    assert(compareBufferContents(buffer6, compareVector));
+
+    buffer6.push(1);
+    buffer6.push(2);
+    compareVector = std::vector({2, 1, 10});
+    assert(compareBufferContents(buffer6, compareVector));
+
+    int exampleVar = buffer6[2];
+    assert(exampleVar == 10);
+
+    buffer6.push(42);
+    buffer6.push(404);
+    compareVector = std::vector({404, 42, 2});
+    assert(compareBufferContents(buffer6, compareVector));
+
+    std::cout << "Array tests passed." << std::endl;
+}
+
+void testVectorBased() {
     // Size is correct.
     CircularBuffer<int> buffer1(2024);
     assert(buffer1.size() == 2024);
@@ -73,7 +146,16 @@ int main() {
     compareVector = std::vector({404, 42, 2});
     assert(compareBufferContents(buffer6, compareVector));
 
+    std::cout << "Vector tests passed." << std::endl;
+}
+
+int main() {
+    testArrayBased();
+
+    testVectorBased();
+
     std::cout << "All tests passed." << std::endl;
 
     return 0;
 }
+
